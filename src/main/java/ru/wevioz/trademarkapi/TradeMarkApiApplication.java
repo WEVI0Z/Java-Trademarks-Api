477eaa1f5b0ba15e6a959c70d5961f7b79f2b292
wevioz <wevioz@mail.ru>
2023-07-12 15:38:35 +0300

    Initial commit

---
+++ src/main/java/ru/wevioz/trademarkapi/TradeMarkApiApplication.java
@@ -0,0 +1,13 @@
+package ru.wevioz.trademarkapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradeMarkApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradeMarkApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/wevioz/trademarkapi/TradeMarkApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.wevioz.trademarkapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TradeMarkApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
