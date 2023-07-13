33dc1a554ac6aad4aba778f604d0707fa587db8b
wevioz <wevioz@mail.ru>
2023-07-13 10:55:02 +0300

    Swagger documentation improved

--- src/main/java/ru/wevioz/trademarkapi/controller/TrademarkController.java
+++ src/main/java/ru/wevioz/trademarkapi/controller/TrademarkController.java
@@ -1,6 +1,9 @@
 package ru.wevioz.trademarkapi.controller;
 
 import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
@@ -18,31 +21,49 @@ import java.util.List;
 public class TrademarkController {
     @Autowired private TrademarkService trademarkService;
 
+    @Operation(summary = "Fill the Database with local trademarks", description = "Returns all the added trademarks")
+    @ApiResponse(responseCode = "200", description = "Success")
     @PostMapping("/fill")
     public List<TrademarkDto> fill() throws IOException {
         return trademarkService.fillDb();
     }
 
+    @Operation(summary = "Get all the trademarks", description = "Returns all the trademarks")
+    @ApiResponse(responseCode = "200", description = "Success")
     @GetMapping()
     public List<TrademarkDto> getAll() {
         return trademarkService.getAll();
     }
 
+    @Operation(summary = "Get the trademark by id", description = "Returns the trademark")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found - the trademark was not found")
+    })
     @GetMapping("/{id}")
     public TrademarkDto get(@PathVariable int id) {
         return trademarkService.getById(id);
     }
 
+    @Operation(summary = "Get the trademark by MarkFeature field", description = "Returns the trademark")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "Success"),
+            @ApiResponse(responseCode = "404", description = "Not found - the trademark was not found")
+    })
     @GetMapping("/mark/{mark}")
     public TrademarkDto getByNumber(@PathVariable String mark) {
         return trademarkService.getByMark(mark);
     }
 
+    @Operation(summary = "Find the closest trademarks by MarkFeature field", description = "Returns the trademarks list")
+    @ApiResponse(responseCode = "200", description = "Success")
     @GetMapping("/mark/find/{mark}")
     public List<TrademarkDto> findByMark(@PathVariable String mark) {
         return trademarkService.findByMark(mark);
     }
 
+    @Operation(summary = "Get all the trademarks in a short form", description = "Returns all the trademarks in a short form")
+    @ApiResponse(responseCode = "200", description = "Success")
     @GetMapping("/short")
     public List<TrademarkShortDto> findByMark() {
         return trademarkService.getShortTrademarks();
